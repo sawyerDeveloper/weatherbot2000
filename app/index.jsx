d5c3a5c22b2eab5404f0c412ee453a30bae5f7b0
unisparkAdmin <sawyer.developer@gmail.com>
2017-06-23 17:39:00 -0400

    updated routing

--- app/index.jsx
+++ app/index.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Weather from './containers/Weather.jsx';
 import Details from './containers/Details.jsx';
+import App from './containers/App.jsx';
 import { browserHistory, Router, Route } from 'react-router';
 
 
 ReactDOM.render((
     <Router history={browserHistory}>
-        <Route path="/" component={Weather}>
-            <Route path="/details" component={Details}/>
+        <Route path="/" component={App}>
+            <Route path="/weather" component={Weather}>
+                <Route path="/details" component={Details}/>
+            </Route>
         </Route>
     </Router>
 ), document.getElementById('content'));
