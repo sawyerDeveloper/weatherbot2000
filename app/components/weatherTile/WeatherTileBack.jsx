3f3b47bc16ca803070b9c0f8740b69574089c8e6
unisparkAdmin <sawyer.developer@gmail.com>
2017-07-21 15:11:48 -0400

    refactored tilefront

---
+++ app/components/weatherTile/WeatherTileBack.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import reactCSS from 'reactcss';
+import HourlyDetail from './HourlyDetail.jsx';
+
+export default class WeatherTileBack extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+
+    render() {
+        if(this.props.hourly == null){
+            console.log('should not render DayDetails')
+            return null;
+        }
+        const dayMapping = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+        const styles = reactCSS({
+        'default': {
+                container: {
+                    borderRadius: 5,
+                    background: 'white',
+                    textAlign: 'center',
+                    width: 360,
+                    height: 350,
+                    position: 'absolute',
+                    left: -100,
+                    top: -100,
+                },
+                header: {
+                    fontSize: '18px',
+                    fontWeight: 'bold',
+                }
+            },
+        })
+
+        let dayName = dayMapping[this.props.dayNum];
+
+        return (
+
+            <div style={ styles.container }>
+                <div style={ styles.header }>
+                    10 Hour Forecast for {dayName}
+                </div>
+                {this.props.hourly.map((hour) => {
+                        return (
+                            <HourlyDetail hour={hour} key={hour.time}/>
+                        );
+                    })}
+            </div>
+        );
+    }
+}
---
+++ app/components/weatherTile/WeatherTileFront.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import reactCSS from 'reactcss';
+import { dayMapping, iconMapping } from '../../utils/constants';
+import ReactAnimatedWeather from 'react-animated-weather';
+
+const WeatherTileFront = (props) => {
+
+    const styles = reactCSS({
+        'default': {
+                container: {
+                    borderRadius: '5px',
+                    width: '100px',
+                    height: '100px',
+                    background: 'white',
+                    textAlign: 'center'
+                },
+                text: {
+                    fontSize: '11px',
+                },
+                dayText: {
+                    fontSize: '13px',
+                    fontWeight: 'bold'
+                }
+            },
+        })
+
+    return (
+        <div style={ styles.container }>
+            <div style={ styles.text }>
+                <div style={ styles.dayText }>{dayMapping[props.dayNum]}</div>
+                <div>Temp {Math.round(props.day.temperatureMax)}&#176;</div>
+                <div>Precip {Math.trunc(props.day.precipProbability * 100)}%</div>
+                <div>Humid {Math.round(props.day.humidity * 100)}%</div>
+            </div>
+            <ReactAnimatedWeather
+                icon={iconMapping[props.day.icon]}
+                color='black'
+                size={32}
+                animate={true}
+            />
+        </div>
+        );
+}
+export default WeatherTileFront
